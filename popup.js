456b74c1ff7b11c0ab5ec0540c0dfb736a80df87
Troy Nguyen <ngutro01@CSGICORP.COM>
2018-09-23 10:01:06 -0500

    added additional button colors and a REVERT button

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 var changeGreen = document.getElementById('changeGreen');
+var originalBackgroundColor = document.body.style.backgroundColor;
 
 changeGreen.onclick = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -7,3 +8,33 @@ changeGreen.onclick = function (element) {
             { code: 'document.body.style.backgroundColor = "green";' });
     });
 };
+
+var changeBlue = document.getElementById('changeBlue');
+
+changeBlue.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: 'document.body.style.backgroundColor = "blue";' });
+    });
+};
+
+var changeOrange = document.getElementById('changeOrange');
+
+changeOrange.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: 'document.body.style.backgroundColor = "orange";' });
+    });
+};
+
+var revert = document.getElementById('revert');
+
+revert.onclick = function (element) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: 'document.body.style.backgroundColor = "' + originalBackgroundColor + '";' });
+    });
+}
